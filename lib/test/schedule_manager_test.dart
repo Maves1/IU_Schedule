213dbeec3a0e8b91582b10a77abde5192c723e7a
Maves1 <seva21122@gmail.com>
2023-05-03 19:58:33 +0300

    [test] Fix test package import

--- lib/test/schedule_manager_test.dart
+++ lib/test/schedule_manager_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:inno_schedule/schedule_manager/schedule_manager.dart';
 import 'package:inno_schedule/schedule_manager/group.dart';
 
