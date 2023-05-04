eb830be0b22ea38da6fdb0701455f959c945896c
Maves1 <seva21122@gmail.com>
2023-05-04 19:51:00 +0300

    Improve network error UI

--- lib/error_placeholder.dart
+++ lib/error_placeholder.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:inno_schedule/schedule_manager/schedule_manager.dart';
 
 Widget errorPlaceholder(ScheduleManager scheduleManager) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: <Widget>[
-      const Center(
-        child: Text("It seems there are problems with the Internet connection"),
-      ),
-      Center(
-        child: ElevatedButton(
-            onPressed: () => scheduleManager.loadSchedule(),
-            child: const Text("Retry")),
-      )
-    ],
+  return Expanded(
+    child: Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.wifi_off_outlined, size: 50),
+        const Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Text("No Internet Connection")),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+          child: ElevatedButton(
+              onPressed: () => scheduleManager.loadSchedule(),
+              child: const Text("Retry")),
+        )
+      ],
+    )),
   );
 }
